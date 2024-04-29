9e85773e1e7ae8803fadc58fae7626e2e8f39977
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-04-29 13:14:50 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,16 +2,16 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
+    OWNER_ID = "6584789596"
+    sudo_users = "6101457748", "6100011620", "6154972031", "6584789596"
     GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
+    TOKEN = "6462620320:AAGIBSjD_PmyiSNPmF3kdYsO0G2TSMpQe9M"
+    mongo_url = "mongodb+srv://Srikanta:srikanta@cluster0.xzbil3m.mongodb.net/?retryWrites=true&w=majority"
+    PHOTO_URL = ["https://telegra.ph/file/ed23556d07d33db18402d.jpg", "https://telegra.ph//file/e64337bbc6cdac7e6b178.jpg"]
+    SUPPORT_CHAT = "Grabbing_Your_WH_Group"
+    UPDATE_CHAT = "Grabbing_Your_WH_Channel"
+    BOT_USERNAME = "Grabbing_Your_Waifu_Bot"
+    CHARA_CHANNEL_ID = "-1002009998662"
     api_id = 26626068
     api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
 
