14436fcd3736686c48be3432581acc11d5d82f75
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-04-29 13:53:08 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -34,11 +34,10 @@ async def start(update: Update, context: CallbackContext) -> None:
         
         
         caption = f"""
-        ***Heyyyy...***
+         ***Heyyyy...***
 
-***I am An Open Source Character Catcher Bot...​Add Me in Your group.. And I will send Random Characters After.. every 100 messages in Group... Use /guess to.. Collect that Characters in Your Collection.. and see Collection by using /Harem... So add in Your groups and Collect Your harem***
+***I am Grabbing Your Waifu Bot...Add Me in Your group.. And I will send Random Characters After.. every 100 messages in Group... Use /grab to.. Collect that Characters in Your Collection.. and see Collection by using /Harem... So add in Your groups and Collect Your harem***
         """
-        
         keyboard = [
             [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
             [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
@@ -90,10 +89,10 @@ async def button(update: Update, context: CallbackContext) -> None:
     elif query.data == 'back':
 
         caption = f"""
-        ***Hoyyyy...*** ✨
+        ***Heyyyy...***
 
-***I am An Open Source Character Catcher Bot..​Add Me in Your group.. And I will send Random Characters After.. every 100 messages in Group... Use /guess to.. Collect that Characters in Your Collection.. and see Collection by using /Harem... So add in Your groups and Collect Your harem***
-        """
+***I am Grabbing Your Waifu Bot...Add Me in Your group.. And I will send Random Characters After.. every 100 messages in Group... Use /grab to.. Collect that Characters in Your Collection.. and see Collection by using /Harem... So add in Your groups and Collect Your harem***
+            """
 
         
         keyboard = [
