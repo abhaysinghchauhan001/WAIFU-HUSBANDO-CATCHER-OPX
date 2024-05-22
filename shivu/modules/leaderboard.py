cc2a9683dcf4ec474c439183b5a77d576f80791d
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-05-23 00:32:02 +0530

    Update leaderboard.py

--- shivu/modules/leaderboard.py
+++ shivu/modules/leaderboard.py
@@ -72,7 +72,7 @@ async def leaderboard(update: Update, context: CallbackContext) -> None:
     ])
     leaderboard_data = await cursor.to_list(length=10)
 
-    leaderboard_message = "<b>TOP 10 USERS WITH MOST CHARACTERS</b>\n\n"
+    leaderboard_message = "<b>TOP 𝟷𝟶 USERS WITH MOST CHARACTERS</b>\n\n"
 
     for i, user in enumerate(leaderboard_data, start=1):
         username = user.get('username', 'Unknown')
