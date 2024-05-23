78843ec88c50aebd82013a1b112bb28b7f608933
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-05-23 08:13:04 +0530

    Update changetime.py

--- shivu/modules/changetime.py
+++ shivu/modules/changetime.py
@@ -1,43 +1,79 @@
-from pymongo import  ReturnDocument
-from pyrogram.enums import ChatMemberStatus, ChatType
-from shivu import user_totals_collection, shivuu
-from pyrogram import Client, filters
-from pyrogram.types import Message
 
-ADMINS = [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]
+from pymongo import ReturnDocument
+from telegram import Update
+from telegram.ext import CommandHandler, CallbackContext
+from shivu import application, OWNER_ID, user_totals_collection
 
-
-@shivuu.on_message(filters.command("changetime"))
-async def change_time(client: Client, message: Message):
-    
-    user_id = message.from_user.id
-    chat_id = message.chat.id
-    member = await shivuu.get_chat_member(chat_id,user_id)
-        
-
-    if member.status not in ADMINS :
-        await message.reply_text('You are not an Admin.')
-        return
+async def change_time(update: Update, context: CallbackContext) -> None:
+    user = update.effective_user
+    chat = update.effective_chat
 
     try:
-        args = message.command
-        if len(args) != 2:
-            await message.reply_text('Please use: /changetime NUMBER')
+        member = await chat.get_member(user.id)
+        if member.status not in ('administrator', 'creator'):
+            await update.message.reply_text('You do not have permission to use this command.')
             return
 
-        new_frequency = int(args[1])
-        if new_frequency < 5:
-            await message.reply_text('The message frequency must be greater than or equal to 100.')
+        args = context.args
+        if len(args) != 1:
+            await update.message.reply_text('Incorrect format. Please use: /changetime NUMBER')
             return
 
-    
+        new_frequency = int(args[0])
+        if new_frequency < 100:
+            await update.message.reply_text('The message frequency must be greater than or equal to 100.')
+            return
+
+        if new_frequency > 10000:
+            await update.message.reply_text('Thats too much buddy. Use below 10000')
+            return
+
         chat_frequency = await user_totals_collection.find_one_and_update(
-            {'chat_id': str(chat_id)},
+            {'chat_id': str(chat.id)},
             {'$set': {'message_frequency': new_frequency}},
             upsert=True,
             return_document=ReturnDocument.AFTER
         )
 
-        await message.reply_text(f'Successfully changed {new_frequency}')
+        await update.message.reply_text(f'Successfully changed character appearance frequency to every {new_frequency} messages.')
     except Exception as e:
-        await message.reply_text(f'Failed to change {str(e)}')
+        await update.message.reply_text('Failed to change character appearance frequency.')
+
+
+async def change_time_sudo(update: Update, context: CallbackContext) -> None:
+    sudo_user_ids = {6584789596}
+    user = update.effective_user
+
+    try:
+        if user.id not in sudo_user_ids:
+            await update.message.reply_text('You do not have permission to use this command.')
+            return
+
+        args = context.args
+        if len(args) != 1:
+            await update.message.reply_text('Incorrect format. Please use: /changetime NUMBER')
+            return
+
+        new_frequency = int(args[0])
+        if new_frequency < 1:
+            await update.message.reply_text('The message frequency must be greater than or equal to 1.')
+            return
+
+        if new_frequency > 10000:
+            await update.message.reply_text('Thats too much buddy. Use below 10000')
+            return
+
+        chat_frequency = await user_totals_collection.find_one_and_update(
+            {'chat_id': str(update.effective_chat.id)},
+            {'$set': {'message_frequency': new_frequency}},
+            upsert=True,
+            return_document=ReturnDocument.AFTER
+        )
+
+        await update.message.reply_text(f'Successfully changed character appearance frequency to every {new_frequency} messages.')
+    except Exception as e:
+        await update.message.reply_text('Failed to change character appearance frequency.')
+
+
+application.add_handler(CommandHandler("ctime", change_time_sudo, block=False))
+application.add_handler(CommandHandler("changetime", change_time, block=False))
