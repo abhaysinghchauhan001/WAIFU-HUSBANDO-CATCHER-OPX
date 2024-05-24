1b5583e88064ead894b865b66a20a0e46f9cb35d
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-05-24 21:36:12 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,12 +5,12 @@ class Config(object):
     OWNER_ID = "6584789596"
     sudo_users = "6584789596", "2010819209", "5702598840", "6764240805", "6101457748", "6154972031", "6100011620", "6412447141"
     GROUP_ID = -1002000314620
-    TOKEN = "6600186454:AAH3b9v9M_K9dgHT5uVpwUMQ9oDyIddXjzM"
+    TOKEN = "6639816047:AAEzylf3eyWcBvgtTJ3xswm47ye-npZUcgo"
     mongo_url = "mongodb+srv://Srikanta:srikanta@cluster0.xzbil3m.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/ed23556d07d33db18402d.jpg", "https://telegra.ph//file/e64337bbc6cdac7e6b178.jpg"]
     SUPPORT_CHAT = "Grabbing_Your_WH_Group"
     UPDATE_CHAT = "FLEX_BOTS_NEWS"
-    BOT_USERNAME = "Grabbing_Your_Waifu_Bot"
+    BOT_USERNAME = "Grabbing_Your_Waifu_XBot"
     CHARA_CHANNEL_ID = "-1002009998662"
     api_id = 24089031
     api_hash = "0615e3afe13ddaaf8e9ddbd3977d35ff"
