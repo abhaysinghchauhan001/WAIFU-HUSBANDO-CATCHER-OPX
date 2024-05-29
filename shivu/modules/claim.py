a6f979f593d0c62c04c627ebb72ef7d72452eccb
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-05-29 11:21:23 +0530

    Update claim.py

--- shivu/modules/claim.py
+++ shivu/modules/claim.py
@@ -1,11 +1,27 @@
 import asyncio
 from pyrogram import filters, Client, types as t
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from shivu import shivuu as bot
 from shivu import user_collection, collection
-import time
 from datetime import datetime, timedelta
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 
-DEVS = (6584789596)
+
+DEVS =  (6584789596) # Devloper user IDs
+SUPPORT_CHAT_ID = -1002000314620  # Change this to your group's chat ID
+
+keyboard = InlineKeyboardMarkup([
+    [InlineKeyboardButton("Join Chat To Use Me", url="https://t.me/Grabbing_Your_WH_Group")],
+    [InlineKeyboardButton("Join Chat To Use Me", url="https://t.me/Flex_Support_Chat")]
+])
+
+
+# Functions from the second code
+async def claim_toggle(claim_state):
+    try:
+        await collection.update_one({}, {"$set": {"claim": claim_state}}, upsert=True)
+    except Exception as e:
+        print(f"Error in claim_toggle: {e}")
 
 async def get_unique_characters(receiver_id, target_rarities=['🟢 Common', '🟣 Rare', '🟡 Legendary']):
     try:
