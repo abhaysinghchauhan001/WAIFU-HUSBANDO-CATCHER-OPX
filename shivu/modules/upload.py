7a83dc8a7eb0444bc1861faa7121675634566479
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-06-07 01:12:15 +0530

    Update upload.py

--- shivu/modules/upload.py
+++ shivu/modules/upload.py
@@ -69,7 +69,7 @@ async def upload(update: Update, context: CallbackContext) -> None:
             message = await context.bot.send_photo(
                 chat_id=CHARA_CHANNEL_ID,
                 photo=args[0],
-                caption=f"<b>{["anime"]}</b>\n<b>{["id"]}:</b> <b>{["character_name"]} [💞]</b>\n(🎐<b>Rarity:</b> {["rarity]"})\n\n💞<b>Valentine</b>💞",
+                caption=f'<b>Character Name:</b> {character_name}\n<b>Anime Name:</b> {anime}\n<b>Rarity:</b> {rarity}\n<b>ID:</b> {id}\n\n👘𝑲𝒊𝒎𝒐𝒏𝒐👘\n\n𝑨𝒅𝒅𝒆𝒅 𝑩𝒚 ➥ <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
                 parse_mode='HTML'
             )
             character['message_id'] = message.message_id
