fddcb12e98a81cd80bad81e3c4982604a0b86d4e
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-06-06 16:52:29 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -4,14 +4,14 @@ class Config(object):
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6584789596"
     sudo_users = "6584789596", "2010819209", "5702598840", "6764240805", "6101457748", "6154972031", "6100011620", "6412447141"
-    GROUP_ID = -1002000314620
+    GROUP_ID = -1002214016605
     TOKEN = "6639816047:AAERIAvgZV2iJQMqmTw1l1_9ZBhuigyOTiE"
-    mongo_url = "mongodb+srv://Srikanta:srikanta@cluster0.xzbil3m.mongodb.net/?retryWrites=true&w=majority"
+    mongo_url = "mongodb+srv://Epic2:w85NP8dEHmQxA5s7@cluster0.tttvsf9.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/ed23556d07d33db18402d.jpg", "https://telegra.ph//file/e64337bbc6cdac7e6b178.jpg"]
     SUPPORT_CHAT = "Grabbing_Your_WH_Group"
     UPDATE_CHAT = "FLEX_BOTS_NEWS"
     BOT_USERNAME = "Grabbing_Your_Waifu_XBot"
-    CHARA_CHANNEL_ID = "-1002009998662"
+    CHARA_CHANNEL_ID = "-1002214016605"
     api_id = 24089031
     api_hash = "0615e3afe13ddaaf8e9ddbd3977d35ff"
 
