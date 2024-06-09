667969a96b5cb444f35fe692159f696fa764837d
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-06-09 09:06:38 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -227,7 +227,7 @@ async def fav(update: Update, context: CallbackContext) -> None:
     user['favorites'] = [character_id]
 
     
-          await user_collection.update_one({'id': user_id}, {'$set': {'favorites': user[favorites']}}
+          await user_collection.update_one({'id': user_id}, {'$set': {'favorites': user['favorites']}}
         img_urls = [character['img_url'] for character in favorites_characters])
 
     await update.message.reply_text(f'**Cʜᴀʀᴀᴄᴛᴇʀ {character["name"]} ʜᴀs ʙᴇᴇɴ ᴀᴅᴅᴇᴅ ᴛᴏ ʏᴏᴜʀ ғᴀᴠᴏʀɪᴛᴇ...**')
