ee01daffb7fca7023726c02694fd3b16addd9963
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-06-19 17:15:09 +0530

    Update trade.py

--- shivu/modules/trade.py
+++ shivu/modules/trade.py
@@ -167,8 +167,8 @@ async def gift(client, message):
 
     await message.reply_text(
 
-photo= args[0]
-f"do You Really Wanns To Gift {message.reply_to_message.from_user.mention} ?", reply_markup=keyboard)
+               photo=args[0]
+               caption=f"do You Really Wanns To Gift {message.reply_to_message.from_user.mention} ?", reply_markup=keyboard)
 
 @shivuu.on_callback_query(filters.create(lambda _, __, query: query.data in ["confirm_gift", "cancel_gift"]))
 async def on_callback_query(client, callback_query):
