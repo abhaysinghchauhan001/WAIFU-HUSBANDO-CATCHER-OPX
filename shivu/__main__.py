1acdf875bfe26a0d8c1d6af5ffeb8c010be4ddd3
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-06-20 18:04:07 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -272,9 +272,10 @@ def main() -> None:
 
     application.add_handler(CommandHandler(["grab"], guess, block=False))
     application.add_handler(CommandHandler("fav", fav, block=False))
+ application.add_handler(MessageHandler(filters.ALL, message_counter, block=False))
 
 application.add_handler(CallbackQueryHandler(handle_favorite_choice))
-    application.add_handler(MessageHandler(filters.ALL, message_counter, block=False))
+   
     application.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
