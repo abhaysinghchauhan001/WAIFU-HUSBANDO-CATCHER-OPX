a82672104dc010405489c6c1bdfb4174df100df9
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-09-21 20:45:02 +0530

    Update database.py

--- shivu/database.py
+++ shivu/database.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient, WriteConcern
 
 # Replace with your actual connection string
-connection_string = "mongodb://myUser:myPassword@localhost:27017/mydb"
+connection_string = "mongodb+srv://Epic2:w85NP8dEHmQxA5s7@cluster0.tttvsf9.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(connection_string)
 
 # Specify your database
-db = client.mydb  # Change 'mydb' to your database name
+db = character_catcherr # Change 'mydb' to your database name
 
 # Access collections
 user_collection = db.user_collection.with_options(write_concern=WriteConcern(w=1))
