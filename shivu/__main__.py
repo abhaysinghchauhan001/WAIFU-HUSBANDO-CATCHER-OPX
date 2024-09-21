8f0b5b828226bba8445d25f6dfcbb2f0e8703cf8
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-09-21 20:59:39 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -1,4 +1,4 @@
-
+from shivu.database import user_collection
 import importlib
 import time
 import random
@@ -11,7 +11,7 @@ from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filters
 from pymongo import WriteConcern
-from shivu.database import user_collection
+
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext, CallbackQueryHandler
 
