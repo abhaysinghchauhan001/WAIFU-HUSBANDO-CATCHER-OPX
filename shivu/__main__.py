dd882bc78f69cd966682b929d69978c76e7f5272
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-09-24 02:45:38 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -219,7 +219,6 @@ def main() -> None:
     """Run bot."""
     # Add handlers
     application.add_handler(CommandHandler(["grab"], guess, block=False))
-    application.add_handler(CommandHandler("hfav", fav, block=False))
     application.add_handler(CallbackQueryHandler(button))
 
     application.run_polling(drop_pending_updates=True)
